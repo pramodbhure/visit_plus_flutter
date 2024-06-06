0baead4d6f9bd226a674fbd74e5610ed9e57bcd4
Priyankabaride <baride.priyanka@gmail.com>
2024-06-06 15:36:06 +0530

    Added doctor profile page

---
+++ lib/screens/bottom_navigation_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: '',
+        ),
+      ],
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.grey,
+    );
+  }
+}
